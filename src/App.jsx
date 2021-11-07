0a9122e724d9f22711e79884aa3ed6e6ad5e42ea
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-11-08 00:57:41 +0100

    clockWithRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import {DigitalClock, CountDown, StopWatch} from './components';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+
 
 const App = () => {
   return (
-    <div class="App">
-      <DigitalClock />
-      <CountDown />
-      <StopWatch />
-    </div>
+    <Router >
+      <header class="header__route">
+        <Link to="/">
+          <a href>Reloj</a>
+        </Link>
+        <Link to="/countdown">
+          <a href>Cuenta Atrás</a>
+        </Link>
+        <Link to="/stopwatch">
+          <a href>Cronómetro</a>
+        </Link>
+    </header>
+    <main class="App">
+      <Route class="clockbutton" exact path="/" component={DigitalClock} />
+      <Route path="/countdown" component={CountDown} />
+      <Route path="/stopwatch" component={StopWatch} />
+    </main>
+    </Router>
   );
 }
 
